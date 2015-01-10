d46b37fd5a5be101af2fcfa6471a3341e9b9b9e1
Yang Li <idealhack@gmail.com>
2015-01-11 00:52:10 +0800

    fix homebrew install script location, again

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # homebrew
 echo -e "\033[0;33mInstalling Homebrew...\033[0m"
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # development tools
 echo -e "\033[0;33mInstalling development tools...\033[0m"
